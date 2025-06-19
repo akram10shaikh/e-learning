cef8292e32a43629171c1c8763b8679cf06429d2
akram <akram10shaikh@gmail.com>
2025-06-19 23:30:27 +0530

    commit

--- app/forms.py
+++ app/forms.py
@@ -82,7 +82,7 @@ class PasswordChangeForm(DjangoPasswordChangeForm):
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'form-control', 'placeholder': self.fields[field].label})
 
-            })
+            
 
 from datetime import date
 
